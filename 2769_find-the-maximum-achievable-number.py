7574e27fff90024bf18bbd299a55d60effd67c1d
Mun Jin <miinjunn@gmail.com>
2024-08-21 20:21:21 +0700

    added ans for 2769

---
+++ 2769_find-the-maximum-achievable-number.py
@@ -0,0 +1,16 @@
+# num, t = 4, 1
+num, t = 3, 2
+
+for _ in range(t):
+    x = (num+1) + 1
+    num = x
+
+print(f'x: {x}')
+
+
+# OR
+
+# angka, lp = 4, 1
+angka, lp = 3, 2
+result = angka + lp*2
+print(f'result: {result}')
