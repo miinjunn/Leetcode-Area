c548a24ba3338fab468b6b24c0f7f95f5baaf4fd
Mun Jin <miinjunn@gmail.com>
2024-09-07 14:25:17 +0700

    fixed LC 69

--- 69_sqrt.py
+++ 69_sqrt.py
@@ -1,8 +1,21 @@
 # im sorry, this is not what the description asked
 def mySqrt(x: int) -> int:
     return int(x**0.5)
+# ------------------------------------------------------
 
 
-# test
+# this is what the description asked
+def mySqrt(x: int) -> int:
+    counter = 1
+    while counter * counter < x:
+        counter += 1
+    if counter * counter == x:
+        return counter
+    else:
+        return counter-1
+
+
+# # test
 print(mySqrt(4))
 print(mySqrt(8))
+print(mySqrt(1))
