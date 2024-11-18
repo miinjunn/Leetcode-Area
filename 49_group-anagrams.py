4ac8b73078d26ec57f171077ceb851852dcefc8e
Mun Jin <miinjunn@gmail.com>
2024-11-18 23:41:55 +0700

    updated minor LC-49

--- 49_group-anagrams.py
+++ 49_group-anagrams.py
@@ -5,29 +5,24 @@ from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # BASIC -------------------------------
-        # def anagram(x, y):
-        #     return sorted(x) == sorted(y)
-
-        # new_strs = [''.join(sorted(i)) for i in strs]
-        # data = dict(Counter(new_strs))
-        # res = [[] for _ in range(len(data))]
-
-        # # print(f'data: {data}')
-
-        # for i in strs:
-        #     for ky, vl in data.items():
-        #         if anagram(i, ky):
-        #             res[list(data).index(ky)].append(i)
-
-        # return res
-
-        # OPTIMIZED -------------------------------
-        data = defaultdict(list)
+        data = dict()
         for i in strs:
-            key = tuple(sorted(i))
-            data[key].append(i)
+            urut = tuple(sorted(i))
+
+            if urut in data:
+                data[urut].append(i)
+            else:
+                data[urut] = [i]
 
         return list(data.values())
+
+        # OPTIMIZED -------------------------------
+        # data = defaultdict(list)
+        # for i in strs:
+        #     key = tuple(sorted(i))
+        #     data[key].append(i)
+
+        # return list(data.values())
 
 
 if __name__ == "__main__":
