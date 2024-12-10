3a7ee9bbf30c23edf1eb3f5016ae911276bea8b8
Mun Jin <miinjunn@gmail.com>
2024-12-10 12:24:09 +0700

    added LC 260

---
+++ 260_single-number-iii.py
@@ -0,0 +1,29 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        n = set(nums)
+        result = []
+        for i in n:
+            if nums.count(i) <= 1:
+                result.append(i)
+        return result
+
+
+if __name__ == "__main__":
+    test = Solution()
+
+    nums1 = [1, 2, 1, 3, 2, 5]
+    # Output: [3,5]
+
+    nums2 = [-1, 0]
+    # Output: [-1,0]
+
+    nums3 = [0, 1]
+    # Output: [1,0]
+
+    print(test.singleNumber(nums1))
+    print(test.singleNumber(nums2))
+    print(test.singleNumber(nums3))
