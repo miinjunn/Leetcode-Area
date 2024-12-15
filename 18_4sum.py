84bb18a7e687ef8b9aad3ba5b020dc7fbe822d2f
Mun Jin <miinjunn@gmail.com>
2024-12-15 14:59:41 +0700

    optimized with two-pointer for LC 18

--- 18_4sum.py
+++ 18_4sum.py
@@ -1,14 +1,55 @@
 from typing import List
-from itertools import combinations
+# from itertools import combinations
 
 
 class Solution:
+    # basic method ------------------------
+    # def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    #     temp = combinations(sorted(nums), 4)
+    #     result = []
+    #     for i in temp:
+    #         if i not in result and sum(i) == target:
+    #             result.append(i)
+    #     return result
+
+    # optimize (two pointer) method ------------------------
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        temp = combinations(sorted(nums), 4)
+        nums.sort()
         result = []
-        for i in temp:
-            if i not in result and sum(i) == target:
-                result.append(i)
+        n = len(nums)
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left, right = j + 1, n - 1
+
+                while left < right:
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if current_sum < target:
+                        left += 1
+                    elif current_sum > target:
+                        right -= 1
+                    else:
+                        result.append(
+                            [nums[i], nums[j], nums[left], nums[right]])
+
+                        # Skip duplicate for 3rd number
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+
+                        # Skip duplicate for 4th number
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+
         return result
 
 
@@ -27,6 +68,6 @@ if __name__ == "__main__":
     target3 = 4
     # Output: [[-5,0,4,5],[-3,-2,4,5]]
 
-    # print(test.fourSum(nums1, target1))
-    # print(test.fourSum(nums2, target2))
+    print(test.fourSum(nums1, target1))
+    print(test.fourSum(nums2, target2))
     print(test.fourSum(nums3, target3))
