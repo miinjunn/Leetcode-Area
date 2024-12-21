7d769015743dc822599108a638779e961c345652
Mun Jin <miinjunn@gmail.com>
2024-12-21 23:09:23 +0700

    added LC 45

--- 45_jump-game-ii.py
+++ 45_jump-game-ii.py
@@ -3,37 +3,50 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        # basic knowledge (still fail)---------------------------------------------
+        # n = len(nums)
+
+        # counter = 0
+        # posisi_idx = 0
+        # sekarang = nums[posisi_idx]
+        # temp = 0
+
+        # while True:
+        #     if posisi_idx == n-1:
+        #         break
+
+        #     temp = max(temp, posisi_idx + sekarang + 1)
+
+        #     if temp >= n-1:
+        #         counter += 1
+        #         break
+
+        #     sekarang = max(nums[posisi_idx + 1: temp])
+        #     asu = nums[posisi_idx + 1:temp]
+        #     posisi_idx = (
+        #         len(asu) - asu[::-1].index(max(asu)) - 1) + posisi_idx + 1
+
+        #     counter += 1
+
+        #     # print(f'temp: {temp}')
+        #     # print(f'sekarang: {sekarang}')
+        #     # print(f'posisi_idx: {posisi_idx}')
+        #     # print(f'counter: {counter}\n')
+
+        # return counter
+
+        # optimize ---------------------------------------------
         n = len(nums)
-        # print(f"n: {n}")
-
-        counter = 0
-        posisi_idx = 0
-        sekarang = nums[posisi_idx]
+        total_jump = 0
+        sekarang = 0
         temp = 0
+        for i in range(0, n-1):
+            temp = max(temp, i+nums[i])
+            if i == sekarang:
+                total_jump += 1
+                sekarang = temp
 
-        while True:
-            if posisi_idx == n-1:
-                break
-
-            temp = max(temp, posisi_idx + sekarang + 1)
-
-            if temp >= n-1:
-                counter += 1
-                break
-
-            sekarang = max(nums[posisi_idx + 1: temp])
-            asu = nums[posisi_idx + 1:temp]
-            posisi_idx = (
-                len(asu) - asu[::-1].index(max(asu)) - 1) + posisi_idx + 1
-
-            counter += 1
-
-            print(f'temp: {temp}')
-            print(f'sekarang: {sekarang}')
-            print(f'posisi_idx: {posisi_idx}')
-            print(f'counter: {counter}\n')
-
-        return counter
+        return total_jump
 
 
 if __name__ == "__main__":
@@ -57,9 +70,9 @@ if __name__ == "__main__":
     nums4 = [2, 3, 2, 2, 1, 3, 0]
     # Output: salah = 5, benar = 4
 
-    # print(test.jump(nums00))
-    # print(test.jump(nums0))
-    # print(test.jump(nums1))
+    print(test.jump(nums00))
+    print(test.jump(nums0))
+    print(test.jump(nums1))
     print(test.jump(nums2))
-    # print(test.jump(nums3))
-    # print(test.jump(nums4))
+    print(test.jump(nums3))
+    print(test.jump(nums4))
