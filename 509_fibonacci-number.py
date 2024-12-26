13fbec10562c5a959750f38f648e49620571ef35
miinjunn <miinjunn@gmail.com>
2024-12-26 15:23:23 +0700

    added LC 509

--- 509_fibonacci-number.py
+++ 509_fibonacci-number.py
@@ -1,12 +1,22 @@
 class Solution:
     # basic method ----------------------------------------
+    # def fib(self, n: int) -> int:
+    #     if n == 0:
+    #         return 0
+    #     if n == 1:
+    #         return 1
+    #     else:
+    #         return self.fib(n-1) + self.fib(n-2)
+
+    # optimized method ----------------------------------------
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        else:
-            return self.fib(n-1) + self.fib(n-2)
+        f = [0, 1]
+
+        for i in range(2, n+1):
+            f.append(f[i-1] + f[i-2])
+
+        print(f)
+        return f[n]
 
 
 if __name__ == "__main__":
