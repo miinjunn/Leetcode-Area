dd14c445cae6c68b346230a6304ce2497e4fde6d
miinjunn <miinjunn@gmail.com>
2024-12-26 15:20:16 +0700

    added LC 509

---
+++ 509_fibonacci-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    # basic method ----------------------------------------
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+
+
+if __name__ == "__main__":
+    test = Solution()
+
+    n1 = 2
+    # Output: 1
+
+    n2 = 3
+    # Output: 2
+
+    n3 = 4
+    # Output: 3
+
+    print(test.fib(n1))
+    print(test.fib(n2))
+    print(test.fib(n3))
