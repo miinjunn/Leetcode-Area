a8914433d2d584feb64d7e7a400137003cf40af5
miinjunn <miinjunn@gmail.com>
2025-01-23 23:42:38 +0700

    added LC 461

---
+++ 461_hamming-distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    test = Solution()
+
+    x1 = 1
+    y1 = 4
+    # Output: 2
+
+    x2 = 3
+    y2 = 1
+    # Output: 1
+
