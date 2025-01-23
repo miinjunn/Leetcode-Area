2fc24a543ede94cda00f64506f609c9dd01e63c8
miinjunn <miinjunn@gmail.com>
2025-01-24 00:03:06 +0700

    fixed LC 461

--- 461_hamming-distance.py
+++ 461_hamming-distance.py
@@ -1,6 +1,20 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        pass
+        xx = bin(x)[2:]
+        yy = bin(y)[2:]
+        temp = max(len(xx), len(yy))
+        while len(xx) < temp:
+            xx = '0' + xx
+
+        while len(yy) < temp:
+            yy = '0' + yy
+
+        counter = 0
+        for i in range(len(xx)):
+            if xx[i] != yy[i]:
+                counter += 1
+
+        return counter
 
 
 if __name__ == "__main__":
@@ -14,3 +28,13 @@ if __name__ == "__main__":
     y2 = 1
     # Output: 1
 
+    x3 = 2
+    y3 = 5
+    # Output: 3
+
+    # 0010
+    # 0101
+    # 0111
+    print(test.hammingDistance(x1, y1))
+    print(test.hammingDistance(x2, y2))
+    print(test.hammingDistance(x3, y3))
