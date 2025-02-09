c55d072e7e366ad3a580bd303682852a5841c22f
miinjunn <miinjunn@gmail.com>
2025-02-09 12:37:38 +0700

    optimized LC 152

--- 152_maximum-product-subarray.py
+++ 152_maximum-product-subarray.py
@@ -3,12 +3,32 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        # result = nums[0]
+        # for i in range(len(nums) - 1):
+        #     temp = nums[i]
+        #     for j in range(i+1, len(nums)):
+        #         temp *= nums[j]
+        #         result = max(temp, result, nums[j])
+
+        # return result
+
+        # OPTMIZED METHOD
+        if not nums:
+            return 0
+
+        max_product = nums[0]
+        min_product = nums[0]
         result = nums[0]
-        for i in range(len(nums) - 1):
-            temp = nums[i]
-            for j in range(i+1, len(nums)):
-                temp *= nums[j]
-                result = max(temp, result, nums[j])
+
+        for i in range(1, len(nums)):
+            current = nums[i]
+            if current < 0:
+                max_product, min_product = min_product, max_product
+
+            max_product = max(current, max_product * current)
+            min_product = min(current, min_product * current)
+
+            result = max(result, max_product)
 
         return result
 
